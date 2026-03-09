20e5104510eb1aae391b224594f0eccdfea218d0
zenitsuagatsuma8090-arch <zenitsuagatsuma8090@gmail.com>
2026-03-09 23:28:28 +0000

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -15,7 +15,7 @@ global.setting = {
 
 global.info = {
  nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ pairingNumber : '212770495956',
  figlet: 'silanalite', // create a start console display
  nomorwa : '212605784394',
  nameown : 'NOUREDDINE',
